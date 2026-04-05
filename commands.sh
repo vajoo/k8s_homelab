643be887b20be719cf46c0ac7d15c1ec762c501a
Laurin Windhaus <laurin.windhaus@gmail.com>
2026-04-05 11:46:55 +0200

    added cluster role yaml files

---
+++ commands.sh
@@ -0,0 +1,16 @@
+docker build -t k8s-demo-api .   
+docker build -t k8s-demo-frontend . 
+docker run -p 8000:8000 k8s-demo-api
+docker run -p 8080:80 k8s-demo-frontend
+
+docker tag k8s-demo-api vajo/k8s-demo-api:latest
+docker tag k8s-demo-frontend vajo/k8s-demo-frontend:latest
+
+docker push vajo/k8s-demo-api:latest
+docker push vajo/k8s-demo-frontend:latest
+
+docker pull vajo/k8s-demo-api:latest
+docker pull vajo/k8s-demo-frontend:latest
+
+kubectl apply -f k8s/base/cluster_reader.yaml
+kubectl apply -f k8s/base/cluster_reader_binding.yaml
